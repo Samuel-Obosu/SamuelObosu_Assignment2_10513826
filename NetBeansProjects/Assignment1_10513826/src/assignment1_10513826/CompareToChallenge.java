7d30aeb1eca675d6d70d3781932e9ce7403f4b43
Samuel-Obosu <obosusamuel@gmail.com>
2016-02-29 12:15:35 +0000

    CompareToChallenge Class completed

---
+++ NetBeansProjects/Assignment1_10513826/src/assignment1_10513826/CompareToChallenge.java
@@ -0,0 +1,41 @@
+package assignment1_10513826;
+public class CompareToChallenge {
+public static void main(String[] args){
+    System.out.print("Comparing \"come\" with \"coming\" coming produces ");
+    System.out.print("come".compareTo("coming"));
+    System.out.println();
+    System.out.print("Comparing \"sit\" with \"stand\" produces ");
+    System.out.print("sit".compareTo("stand"));
+    System.out.println();
+    System.out.print("Comparing \"desk\" with \"disk\" coming produces ");
+    System.out.print("desk".compareTo("disk"));
+    System.out.println();
+    System.out.print("Comparing \"bite\" with \"kite\" coming produces ");
+    System.out.print("bite".compareTo("kite"));
+    System.out.println();
+    System.out.print("Comparing \"king\" with \"queen\" coming produces ");
+    System.out.print("king".compareTo("queen"));
+    System.out.println();
+    System.out.print("Comparing \"sir\" with \"madam\" coming produces ");
+    System.out.print("sir".compareTo("madam"));
+    System.out.println();
+    System.out.print("Comparing \"throw\" with \"low\" coming produces ");
+    System.out.print("throw".compareTo("low"));
+    System.out.println();
+    System.out.print("Comparing \"wine\" with \"line\" coming produces ");
+    System.out.print("wine".compareTo("line"));
+    System.out.println();
+    System.out.print("Comparing \"seen\" with \"mean\" coming produces ");
+    System.out.print("seen".compareTo("mean"));
+    System.out.println();
+    System.out.print("Comparing \"look\" with \"ant\" coming produces ");
+    System.out.print("look".compareTo("ant"));
+    System.out.println();
+    System.out.print("Comparing \"gone\" with \"gone\" coming produces ");
+    System.out.print("gone".compareTo("gone"));
+    System.out.println();
+    System.out.print("Comparing \"space\" with \"space\" coming produces ");
+    System.out.print("space".compareTo("space"));
+    System.out.println();
+}    
+}
