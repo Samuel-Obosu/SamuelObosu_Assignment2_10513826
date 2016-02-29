a5c2e366dc933d51c9858529c90b12ac67539220
Samuel-Obosu <obosusamuel@gmail.com>
2016-02-29 12:18:28 +0000

    ModulusAnimation Class completed

---
+++ NetBeansProjects/Assignment1_10513826/src/assignment1_10513826/ModulusAnimation.java
@@ -0,0 +1,33 @@
+package assignment1_10513826;
+public class ModulusAnimation {
+public static void main( String[] args ) throws Exception
+	{
+		for ( int i=0; i<80; i++ )
+		{
+			if ( i%10 == 0 )
+				System.out.print(" @@@@@@@@ \r");
+                        else if ( i%11 == 1 )
+				System.out.print(" @@@@@@@@ \r");
+			else if ( i%11 == 2 )
+				System.out.print(" @@@@@@@@ \r");
+			else if ( i%11 == 3 )
+				System.out.print(" @@@@@@@@ \r");
+			else if ( i%11 == 4 )
+				System.out.print(" @@@@@@@@ \r");
+			else if ( i%11 == 5 )
+				System.out.print(" @@@@@@@@ \r");
+			else if ( i%11 == 6 )
+				System.out.print(" @@@@@@@@ \r");
+			else if ( i%11 == 7 )
+				System.out.print(" @@@@@@@@ \r");
+			else if ( i%11 == 8 )
+				System.out.print(" @@@@@@@@ \r");
+			else if ( i%11 == 9 )
+				System.out.print(" @@@@@@@@ \r");
+			else if ( i%11 == 10 )
+				System.out.print(" @@@@@@@@ \r");
+			Thread.sleep(200);
+		}
+		
+	}    
+}
